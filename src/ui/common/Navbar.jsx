fe7f4ae1fafc0a80e85ae2a7f16cf0474d1f7044
mariosepulveda3 <mariosepul3@gmail.com>
2023-01-26 10:08:59 +0100

    ADD nd STYLE: Give more styles to the navbar and implement the hamburger menu.

--- src/ui/common/Navbar.jsx
+++ src/ui/common/Navbar.jsx
@@ -9,6 +9,7 @@ import {
   useBreakpointValue,
   Image,
   Input,
+  Collapse,
 } from "@chakra-ui/react";
 import React, { useState } from "react";
 import { FiUser, FiMenu, FiSearch, FiShoppingCart } from "react-icons/fi";
@@ -20,13 +21,14 @@ export const Navbar = () => {
 
   const [searchTerm, setSearchTerm] = useState("");
   const [showInput, setShowInput] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
   function handleSearch() {
     // hacer la función mamahuevos
   }
 
   return (
-    <Box as="section" w="85%" m="0 auto">
+    <Box as="section" w="85%" m="0 auto" color="#23375B">
       <Box as="nav">
         <Flex justify="space-between" py="30px">
           <HStack>
@@ -34,20 +36,70 @@ export const Navbar = () => {
           </HStack>
           <HStack>
             {isDesktop && (
-              <ButtonGroup spacing="2" p="8px" borderRadius="50px" bg="whitesmoke">
-                <Button as={NavLink} to="/">
+              <ButtonGroup
+                spacing="2"
+                p="8px"
+                borderRadius="50px"
+                bg="whitesmoke"
+              >
+                <Button
+                  as={NavLink}
+                  to="/"
+                  _hover={{
+                    bg: "#23375B",
+                    color: "white",
+                    borderRadius: "50px",
+                    // transform: "scale(1.05)"
+                  }}
+                >
                   Inicio
                 </Button>
-                <Button as={NavLink} to="/courses">
+                <Button
+                  as={NavLink}
+                  to="/courses"
+                  _hover={{
+                    bg: "#23375B",
+                    color: "white",
+                    borderRadius: "50px",
+                    // transform: "scale(1.05)"
+                  }}
+                >
                   Cursos
                 </Button>
-                <Button as={NavLink} to="/freetests">
+                <Button
+                  as={NavLink}
+                  to="/freetests"
+                  _hover={{
+                    bg: "#23375B",
+                    color: "white",
+                    borderRadius: "50px",
+                    // transform: "scale(1.05)"
+                  }}
+                >
                   Tests Gratis
                 </Button>
-                <Button as={NavLink} to="/shop">
+                <Button
+                  as={NavLink}
+                  to="/shop"
+                  _hover={{
+                    bg: "#23375B",
+                    color: "white",
+                    borderRadius: "50px",
+                    // transform: "scale(1.05)"
+                  }}
+                >
                   Tienda
                 </Button>
-                <Button as={NavLink} to="/contact">
+                <Button
+                  as={NavLink}
+                  to="/contact"
+                  _hover={{
+                    bg: "#23375B",
+                    color: "white",
+                    borderRadius: "50px",
+                    // transform: "scale(1.05)"
+                  }}
+                >
                   Contacto
                 </Button>
               </ButtonGroup>
@@ -56,27 +108,31 @@ export const Navbar = () => {
           {isDesktop ? (
             <HStack spacing="4">
               <ButtonGroup color="white" variant="ghost" spacing="1">
-                <IconButton
-                  onClick={() => setShowInput(!showInput)}
-                  icon={<FiSearch fontSize="1.25rem" />}
-                  aria-label="Search"
-                />
-                <Input
-                  value={searchTerm}
-                  onChange={(e) => setSearchTerm(e.target.value)}
-                  placeholder="mamahuevos"
-                  display={showInput ? "block" : "none"}
-                  onKeyPress={(e) => {
-                    if (e.key === "Enter") {
-                      handleSearch();
-                    }
-                  }}
-                />
+                <Flex>
+                  <Input
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                    placeholder="mamahuevos"
+                    display={showInput ? "block" : "none"}
+                    onKeyPress={(e) => {
+                      if (e.key === "Enter") {
+                        handleSearch();
+                      }
+                    }}
+                  />
+                  <IconButton
+                    onClick={() => setShowInput(!showInput)}
+                    icon={<FiSearch fontSize="1.25rem" />}
+                    aria-label="Search"
+                    borderRadius="50px"
+                  />
+                </Flex>
                 <IconButton
                   as={NavLink}
                   to="/cart"
                   icon={<FiShoppingCart fontSize="1.25rem" />}
                   aria-label="Cart"
+                  borderRadius="50px"
                 />
 
                 <IconButton
@@ -85,16 +141,44 @@ export const Navbar = () => {
                   to="/login"
                   icon={<FiUser fontSize="1.25rem" />}
                   aria-label="MyAccount"
+                  borderRadius="50px"
                 />
               </ButtonGroup>
               {/* <Avatar boxSize="10" name="Christoph Winston" src="/* token.user.photo *" /> */}
             </HStack>
           ) : (
-            <IconButton
-              variant="ghost"
-              icon={<FiMenu fontSize="1.25rem" />}
-              aria-label="Open Menu"
-            />
+            <Box display="flex" position="relative">
+              <Collapse
+                in={isOpen}
+                zIndex="9999"
+                style={{ position: "absolute", top: "0px", right: "50px" }}
+              >
+                <Box
+                  display="flex"
+                  alignItems="center"
+                  flexDirection="column"
+                  bg="whitesmoke"
+                  borderRadius="10px"
+                >
+                  <Button>Option 1</Button>
+                  <Button>Option 2</Button>
+                  <Button>Option 3</Button>
+                  <Button>Option 1</Button>
+                  <Button>Option 2</Button>
+                  <Button>Option 3</Button>
+                  <Button>Option 1</Button>
+                  <Button>Option 2</Button>
+                  <Button>Option 3</Button>
+                </Box>
+              </Collapse>
+              <IconButton
+                variant="ghost"
+                icon={<FiMenu fontSize="1.25rem" />}
+                aria-label="Open Menu"
+                relative
+                onClick={() => setIsOpen(!isOpen)}
+              />
+            </Box>
           )}
         </Flex>
       </Box>
