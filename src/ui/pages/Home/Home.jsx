497a6bb3cda2ab84d535302b81ad35a0658884dd
Guillermo Chacón Poveda <guillermo.chacon@bootcamp-upgrade.com>
2023-01-27 18:14:23 +0100

    FEAT: Home

--- src/ui/pages/Home/Home.jsx
+++ src/ui/pages/Home/Home.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import { Box, Center, DarkMode, Flex, Heading, Stack, Text, ButtonGroup, Button } from "@chakra-ui/react";
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Flex>
+   <Box>
+   <Heading
+   >BIENVENID@S</Heading>
+   <Text>Prepárate para mejorar tu escalafón</Text>
+   <Text>
+    En Uniforme Azul te queremos ayudar con nuestros cursos de exámenes
+    tipo test, libros de resúmenes, esquemas y exámenes tipo test en papel
+   </Text>
+   </Box>
+   <ButtonGroup display="flex" flexDirection="column" gap="5px">
+    <Button>
+      Asistente
+    </Button>
+    <Button>
+      Acceder a los cursos
+    </Button>
+    <Button>
+      Acceder a la tienda
+    </Button>
+   </ButtonGroup>
+   </Flex>
   )
 }
 
